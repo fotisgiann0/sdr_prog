fe1dc2796f4f40f5fefcf06c747f1a3f6ee6926b
fotisgiann0 <fotisg77@gmail.com>
2024-02-23 21:41:37 +0200

    changes for smaller numbers

--- approximate.py
+++ approximate.py
@@ -36,18 +36,18 @@ le = 0.5
 lt = 1 - le
 L = 100
 s_elliniko = 150 
-rmin = (400-s_elliniko) * (10**6) 
-rmax = (400+s_elliniko) * (10**6)
+rmin = (400-s_elliniko) #* (10**6) 
+rmax = (400+s_elliniko) #* (10**6)
 m_elliniko = 10
 p_elliniko = 1.25 * (10**(-26))  #-26 kanonika
 z_elliniko = 3
-Ck_ul = np.random.uniform(10,20,size=(m+1,))  #allages edw
-Ck_dl = np.random.uniform(10,20,size=(m+1,))  #kai edw
-Ck_ul[0] = 100000
-Ck_dl[0] = 100000
+Ck_ul = [15, 15, 15]#np.random.uniform(10,20,size=(m+1,))  #allages edw
+Ck_dl = [15, 15, 15]#np.random.uniform(10,20,size=(m+1,))  #kai edw
+# Ck_ul[0] = 100000
+# Ck_dl[0] = 100000
 ai = np.empty((n), float) #make 0.1 0.2 ...
 for i in range(n):
-    ai[i] =  (0.1 + i*0.1) * (10**6)
+    ai[i] =  0.1 + i*0.1 #(0.1 + i*0.1) * (10**6)
 #print("this is ai", ai)
 w = np.empty((1,n), float)
 for i in range(n):
@@ -58,18 +58,22 @@ for i in range(n):
     bi[i] = 0.2 * ai[i]
 r_k = np.empty((m+1), float)
 for i in range(m+1):
-    r_k[i] = 2 * (10**9)
-r_k[0] = 400 * (10**6)
-r_k[1] = 2 * (10**9)
-r_k[2] = 2.2 * (10**9)
+    r_k[i] = 0 #2 * (10**9)
+r_k[0] = 400 #400 * (10**6)
+r_k[1] = 2000#2 * (10**9)
+r_k[2] = 2200#2.2 * (10**9)
 dul = np.empty((n,m+1), float)
 ddl = np.empty((n,m+1), float)
 Dk = np.empty((n,m+1), float)
 #ZEROING THE M = 0 stuff, change ckul ckdl(den xreiazetai aparaitita)
 for i in range(n):
     for j in range(m+1):
-        dul[i][j] = ai[i] / (Ck_ul[j] * (10**6))
-        ddl[i][j] = bi[i] / (Ck_dl[j] * (10**6))
+        if(j == 0):
+            dul[i][0] = 0
+            ddl[i][0] = 0
+        else :
+            dul[i][j] = ai[i] / Ck_ul[j] #(Ck_ul[j] * (10**6))
+            ddl[i][j] = bi[i] / Ck_dl[j] #(Ck_dl[j] * (10**6))
         Dk[i][j] = dul[i][j] + ddl[i][j] + (w[0][i] / r_k[j])
 print(dul)
 print(ddl)
@@ -198,7 +202,7 @@ def initilization():
     A0 = np.block(
         fini
     )
-    #print("this is A0" , len(A0)) looks good
+    print("this is A0" , A0) #looks good
     # a2 einai o a2, g3 einai o adj tou a2
     g2 = []
     g2.append(np.zeros([1,q2 - 2]))
@@ -381,14 +385,16 @@ def initilization():
 
     first_row.append(A0)
     first_row.append(0.5*b0)
+    print("firstr row", first_row)
     final_array.append(first_row)
     second_row.append(0.5*b0t)
     second_row.append(0)
+    print("second row", second_row)
     final_array.append(second_row)
     B00 = np.block(
         final_array
     )
-    print("edw eibai o B0", B00[ :, q+2])
+    print("edw eibai o B0", B00)
 
 
     #b2 einai o b2, g5 einai o adj tou b2
