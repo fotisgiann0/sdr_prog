d8801b74436ede87e17ab28f82c9e92b8919a46f
fotisgiann0 <fotisg77@gmail.com>
2024-03-07 16:51:47 +0200

    changes to mapping, cost function, r0 calculation

--- approximate.py
+++ approximate.py
@@ -3,8 +3,9 @@ import cvxpy as cp
 import numpy as np
 import itertools
 import random
+import sys
 from math import exp, log
-
+np.set_printoptions(threshold=sys.maxsize)
 ###################### basic settings
 m = 2
 n = 10  #kanonika 10
@@ -32,11 +33,11 @@ ksiedge = 0.06706
 alpha = 1 # change from 0.1 to 500 
 ptx = 1.285
 prx = 1.181
-le = 0.001
+le = 0.999
 lt = 1 - le
 L = 100
 s_elliniko = 150 
-rmin = 200#150 #(400-s_elliniko) #* (10**6) 
+rmin = 400#150 #(400-s_elliniko) #* (10**6) 
 rmax = 800#550 #(400+s_elliniko) #* (10**6)
 m_elliniko = 10
 p_elliniko = 1.25 * (10**(-20))  #-26 kanonika
@@ -81,32 +82,32 @@ for i in range(n):
 # print(w[0][2] / r_k[1])
 
 #################### functions
-def gompertz_local (s, remote): # remote == True for edge gompertz function. 
-    if remote == True:
-        a = 0.95
-        b = 5
-        c = 5
-    else: 
-        a = 0.65  #TODO fix this with something that can be tuned
-        b = 5 
-        c = 5
-    g = a*exp(-b*exp(-c*s))
-    return g
+# def gompertz_local (s, remote): # remote == True for edge gompertz function. 
+#     if remote == True:
+#         a = 0.95
+#         b = 5
+#         c = 5
+#     else: 
+#         a = 0.65  #TODO fix this with something that can be tuned
+#         b = 5 
+#         c = 5
+#     g = a*exp(-b*exp(-c*s))
+#     return g
 
-def calculate_hyperplane_approximation(trans_time): #TODO substitute with gompertz variables
-    x_vals = np.linspace(0.0001, 0.94, 50) # generate 50 evenly spaced values between 0 and 0.95
-    y_vals = np.linspace(0.0001, 10, 50) # generate 50 evenly spaced values between 0 and 1
+# def calculate_hyperplane_approximation(trans_time): #TODO substitute with gompertz variables
+#     x_vals = np.linspace(0.0001, 0.94, 50) # generate 50 evenly spaced values between 0 and 0.95
+#     y_vals = np.linspace(0.0001, 10, 50) # generate 50 evenly spaced values between 0 and 1
 
-    x, y = np.meshgrid(x_vals, y_vals) # create a grid of (x,y) points
+#     x, y = np.meshgrid(x_vals, y_vals) # create a grid of (x,y) points
 
-    z = trans_time*0.5*np.log(5/np.log(0.95/x)) - y # compute the function values at each (x,y) point
+#     z = trans_time*0.5*np.log(5/np.log(0.95/x)) - y # compute the function values at each (x,y) point
 
-    A = np.column_stack([x.ravel(), y.ravel(), np.ones_like(x.ravel())]) # create the design matrix
-    coeffs, _, _, _ = np.linalg.lstsq(A, z.ravel(), rcond=None) # compute the coefficients using least squares regression
+#     A = np.column_stack([x.ravel(), y.ravel(), np.ones_like(x.ravel())]) # create the design matrix
+#     coeffs, _, _, _ = np.linalg.lstsq(A, z.ravel(), rcond=None) # compute the coefficients using least squares regression
 
-    a, b, c = coeffs 
+#     a, b, c = coeffs 
 
-    return a,b,c
+#     return a,b,c
 
 #arithmisi apo to 0 sto up
 def create_up(sp):
@@ -132,12 +133,19 @@ def calc_r0(pert):
         return rmin
     for i in range(n):
         sum1 += pert[0][i] * w[0][i]
+    max_for_ru = 0
     for j in range(1,m+1):
+        sum2 = 0.0
         for i in range(n):
             sum2 += pert[0][i+j*n] * Dk[i][j]
+        if(sum2 > max_for_ru):
+            max_for_ru = sum2
     # print("sum1", sum1, type(sum1))
     # print("sum2", sum2, type(sum2))
-    ru = sum1 / sum2
+    if(max_for_ru == 0):
+        return rmin
+        #print("max_for_ru is 0, pert is ", pert)
+    ru = sum1 / max_for_ru
     rl = (lt / (2 * le * p_elliniko)) ** (1/3)
     if ru < rmin:
         r0 = rmin
@@ -448,9 +456,18 @@ def initilization():
     B50 = np.block(
         final_array
     )
-    # print("edw einai o B5 last row", B50[q4])
-    # print("edw einai o B5 last column", B50[:,q4])
-
+    # print("B5 last row", B50[q4])
+    # print("B5 last column", B50[:,q4])
+    # print("o B5", B50)
+    # print("length B5", len(B50))
+    # print("size of B5", B50.size)
+    # print("shape of B5", B50.shape)
+    # counter = 0
+    # for i in range(q4+1):
+    #     for j in range(q4+1):
+    #         if(B50[i][j] != 0):
+    #             counter = counter + 1
+    # print("counter is non zero elements in B5", counter)
     #Gp 
     Gp_ol = []
     for j in range(p):
@@ -544,73 +561,73 @@ def initilization():
    # print("this is jj", Jj_ol[0], Jj_ol[0][0], Jj_ol[0][n*m + n + 3])
     return B00,B20,B40,B50,Gp_ol,Hh_ol,Jj_ol
 
-def scaling_problem(sdr_solution):
-    # print("Start of scaling Problem")
-    x_variables = sdr_solution[0:n]
-    #calculate local average for Latency and accuracy
-    local_executed_requests_count = x_variables.tolist().count(1)
-    if local_executed_requests_count!=0:
-        average_local_latency = sum((ksilocal*x*y+zlocal for x,y in zip(d,x_variables.tolist())))/local_executed_requests_count
-        average_local_accuracy = gompertz_local(1,False)
-    else: 
-        average_local_latency = 0 
-        average_local_accuracy = 0 
+# def scaling_problem(sdr_solution):
+#     # print("Start of scaling Problem")
+#     x_variables = sdr_solution[0:n]
+#     #calculate local average for Latency and accuracy
+#     local_executed_requests_count = x_variables.tolist().count(1)
+#     if local_executed_requests_count!=0:
+#         average_local_latency = sum((ksilocal*x*y+zlocal for x,y in zip(d,x_variables.tolist())))/local_executed_requests_count
+#         average_local_accuracy = gompertz_local(1,False)
+#     else: 
+#         average_local_latency = 0 
+#         average_local_accuracy = 0 
 
 
-    sdr_solution = sdr_solution[n:] # remove first n elements for x_i variables and the last one
+#     sdr_solution = sdr_solution[n:] # remove first n elements for x_i variables and the last one
     
-    y = [sdr_solution[j:j + m] for j in range(0, len(sdr_solution), m)]  # transform 1-d array to 2-d arrays of m columns
-    y = [l.tolist() for l in y]   # list of arrays to list of lists
+#     y = [sdr_solution[j:j + m] for j in range(0, len(sdr_solution), m)]  # transform 1-d array to 2-d arrays of m columns
+#     y = [l.tolist() for l in y]   # list of arrays to list of lists
     
-    # cpvxpy variables
-    u = cp.Variable(m) # regards the variable t of time 
-    w = cp.Variable(n) # regards the substitute of gompertz function 
-    constraints= []
-    objective = 0 
-    # this is to add all log_sum_exp 
-    for i in range (n): 
-        for j in range (m): 
-            objective += y[i][j]*u[j] - (alpha*y[i][j])*w[i] 
-    # this is to add the constraints
+#     # cpvxpy variables
+#     u = cp.Variable(m) # regards the variable t of time 
+#     w = cp.Variable(n) # regards the substitute of gompertz function 
+#     constraints= []
+#     objective = 0 
+#     # this is to add all log_sum_exp 
+#     for i in range (n): 
+#         for j in range (m): 
+#             objective += y[i][j]*u[j] - (alpha*y[i][j])*w[i] 
+#     # this is to add the constraints
 
-    temp = 0 
-    temp1 = 0 
-    for j in range (m): 
-        for i in range (n): 
-            temp += y[i][j] * u[j]    
-            temp1 += y[i][j] * w[i] 
-            constraints += [w[i] >= 0.95*exp(-5)]    #TODO substitute with gompertz variables
-            constraints += [w[i] <= 0.95*exp(-5*exp(-5))]  #TODO substitute with gompertz variables
-            constraints += [cp.exp(u[j]) >= 0.1]
+#     temp = 0 
+#     temp1 = 0 
+#     for j in range (m): 
+#         for i in range (n): 
+#             temp += y[i][j] * u[j]    
+#             temp1 += y[i][j] * w[i] 
+#             constraints += [w[i] >= 0.95*exp(-5)]    #TODO substitute with gompertz variables
+#             constraints += [w[i] <= 0.95*exp(-5*exp(-5))]  #TODO substitute with gompertz variables
+#             constraints += [cp.exp(u[j]) >= 0.1]
 
-            a,b,c = calculate_hyperplane_approximation(d[i] / R[i][j])
-            constraints += [a*w[i]+b*u[j]+c<=0]
+#             a,b,c = calculate_hyperplane_approximation(d[i] / R[i][j])
+#             constraints += [a*w[i]+b*u[j]+c<=0]
 
-            # first_term = (d[i] / R[i][j])* log((exp(1)+1)/2) - exp(-1.15) - (-1.15)*(-exp(-1.15)) - (exp(1)+1)/2*((d[i] / R[i][j])/(exp(1)+1))
-            # constraints += [first_term-cp.exp(-1.15)*u[j] + (d[i] / R[i][j])/(exp(1)+1)*w[i]<=0] 
-            # constraints += [y[i][j] * (d[i] / R[i][j]) * cp.log(w[i]) <= cp.exp(u[j])]#  y[i][j] * (c * d[i] / R[i][j]) * cp.log((-cp.log(a)+w)/b)
-    constraints += [temp <= Lmax*n-average_local_latency*local_executed_requests_count]
-    constraints += [temp1 >= Amin*n-average_local_accuracy*local_executed_requests_count]
-    # objective = cp.log_sum_exp(objective)
-    prob = cp.Problem(cp.Minimize(objective),constraints)
-    # prob.solve(solver=cp.SCS, qcp=True ,low=1, high=5)#, verbose= True)
-    prob.solve(solver="MOSEK",  qcp=True)#, verbose=True)
+#             # first_term = (d[i] / R[i][j])* log((exp(1)+1)/2) - exp(-1.15) - (-1.15)*(-exp(-1.15)) - (exp(1)+1)/2*((d[i] / R[i][j])/(exp(1)+1))
+#             # constraints += [first_term-cp.exp(-1.15)*u[j] + (d[i] / R[i][j])/(exp(1)+1)*w[i]<=0] 
+#             # constraints += [y[i][j] * (d[i] / R[i][j]) * cp.log(w[i]) <= cp.exp(u[j])]#  y[i][j] * (c * d[i] / R[i][j]) * cp.log((-cp.log(a)+w)/b)
+#     constraints += [temp <= Lmax*n-average_local_latency*local_executed_requests_count]
+#     constraints += [temp1 >= Amin*n-average_local_accuracy*local_executed_requests_count]
+#     # objective = cp.log_sum_exp(objective)
+#     prob = cp.Problem(cp.Minimize(objective),constraints)
+#     # prob.solve(solver=cp.SCS, qcp=True ,low=1, high=5)#, verbose= True)
+#     prob.solve(solver="MOSEK",  qcp=True)#, verbose=True)
 
-    # prob.solve(solver="SCS",verbose=True)
-    # Print result.
-    # print("The Convex Program for s optimal value is", prob.value)
-    # print("A solution for u is")
-    # print(u.value)
-    # print("A solution for w is")
-    # print(w.value)
-    # tlist = u.value
+#     # prob.solve(solver="SCS",verbose=True)
+#     # Print result.
+#     # print("The Convex Program for s optimal value is", prob.value)
+#     # print("A solution for u is")
+#     # print(u.value)
+#     # print("A solution for w is")
+#     # print(w.value)
+#     # tlist = u.value
 
-    wlist = w.value
-    slist = []
-    for item in wlist:
-        slist.append(1/5*log(5/(log(0.95/item)))) #TODO include variables of gompertz
-    # print("A solution for s is", slist)
-    return slist
+#     wlist = w.value
+#     slist = []
+#     for item in wlist:
+#         slist.append(1/5*log(5/(log(0.95/item)))) #TODO include variables of gompertz
+#     # print("A solution for s is", slist)
+#     return slist
 
 
 def sdr_offloading(B00,B20,B40,B50,Gp_ol,Hh_ol,Jj_ol):     
@@ -676,7 +693,8 @@ def sdr_offloading(B00,B20,B40,B50,Gp_ol,Hh_ol,Jj_ol):
         #A = np.trace(B40 @ Y)
         # if L < 0 and A == 0 and all([np.trace(Jj_ol[i] @ Y) == 1 for i in range(n)]) and all ([np.trace(Hh_ol[i] @ Y) <= 0 for i in range(m)]) and all ([np.trace(Gp_ol[i] @ Y) == 0 for i in range(p)]):
             #candidate = np.trace(B00 @ Y)
-        candidate = calc_r0(pert)
+        #print("pert here", pert)
+        candidate = calculate_cost(pert[0], calc_r0(pert)) #itan calc_r0(pert)
         if candidate < minimum_obj:
             minimum_obj= candidate
             solution = pert
@@ -709,81 +727,81 @@ def sdr_offloading(B00,B20,B40,B50,Gp_ol,Hh_ol,Jj_ol):
     # print  ("Amax: ", Amax)
     return sdr_solution, optimal_freq
 
-def random_offloading(B0,B1,B2,B3,B4):
-    condition = True
-    while (condition): 
-        condition = False
-        xlist =  [random.randint(0, 1) for _ in range(n)]
-        ylist = [] 
-        for i, val in enumerate(xlist):
-            if val == 0:
-                sublist = [0] * m
-                idx = np.random.randint(0, m)  # choose random index to set to 1
-                sublist[idx] = 1
-            else:
-                sublist = [0] * m
-            ylist.append(sublist)
-        solution = np.concatenate((xlist, np.array(ylist).flatten()))
-        for j in range(m):
-            check_bmax = 0 
-            for sublist in ylist:
-                check_bmax += sublist[j]
-            if check_bmax >= bmax[j]:
-                condition= True
-        total_cost, L, A = calculate_cost(solution,B0,B1,B2,B3,B4)
-        if L >Lmax or A <= Amin:
-            condition= True
-        # print (solution)
-    # print (check_bmax)
-    # print (bmax)
-    return solution
+# def random_offloading(B0,B1,B2,B3,B4):
+#     condition = True
+#     while (condition): 
+#         condition = False
+#         xlist =  [random.randint(0, 1) for _ in range(n)]
+#         ylist = [] 
+#         for i, val in enumerate(xlist):
+#             if val == 0:
+#                 sublist = [0] * m
+#                 idx = np.random.randint(0, m)  # choose random index to set to 1
+#                 sublist[idx] = 1
+#             else:
+#                 sublist = [0] * m
+#             ylist.append(sublist)
+#         solution = np.concatenate((xlist, np.array(ylist).flatten()))
+#         for j in range(m):
+#             check_bmax = 0 
+#             for sublist in ylist:
+#                 check_bmax += sublist[j]
+#             if check_bmax >= bmax[j]:
+#                 condition= True
+#         total_cost, L, A = calculate_cost(solution,B0,B1,B2,B3,B4)
+#         if L >Lmax or A <= Amin:
+#             condition= True
+#         # print (solution)
+#     # print (check_bmax)
+#     # print (bmax)
+#     return solution
 
 def random_compression(): 
     slist = np.random.uniform(0.1,1,size=(n,))
     return slist
 
-def offload_local():
-    xlist =  [1]*n
-    ylist = [] 
-    for i, val in enumerate(xlist):
-        sublist = [0] * m
-        ylist.append(sublist)
-    solution = np.concatenate((xlist, np.array(ylist).flatten()))
-    return solution
+# def offload_local():
+#     xlist =  [1]*n
+#     ylist = [] 
+#     for i, val in enumerate(xlist):
+#         sublist = [0] * m
+#         ylist.append(sublist)
+#     solution = np.concatenate((xlist, np.array(ylist).flatten()))
+#     return solution
 
-def offload_remote(): 
-    xlist = [0]*n
-    ylist = [] 
-    for i, val in enumerate(xlist):
-        sublist = [0] * m
-        idx = np.random.randint(0, m)  # choose random index to set to 1
-        sublist[idx] = 1
-        ylist.append(sublist)
-    solution = np.concatenate((xlist, np.array(ylist).flatten()))
-    return solution
+# def offload_remote(): 
+#     xlist = [0]*n
+#     ylist = [] 
+#     for i, val in enumerate(xlist):
+#         sublist = [0] * m
+#         idx = np.random.randint(0, m)  # choose random index to set to 1
+#         sublist[idx] = 1
+#         ylist.append(sublist)
+#     solution = np.concatenate((xlist, np.array(ylist).flatten()))
+#     return solution
 
-def implement_brute_force(B0,B1,B2,B3,B4,B5): 
-    ############## Implement brute force solution....
-    A = lst = [list(i) for i in itertools.product([0, 1], repeat=n+n*m)]
-    minimum_obj = 10000000000000 
-    solution=[]
-    # print (len(A))
-    for pert in A:
-        pert.append(1)
-        pert = np.array([pert])
-        Y = np.transpose(pert)*pert
-        L = np.trace(B1 @ Y)
-        A = np.trace(B2 @ Y)
-        if L < Lmax*n and A <= -Amin * n and all([np.trace(B3[i] @ Y) == 1 for i in range(n)]) and all ([np.trace(B4[i] @ Y) <= bmax[i] for i in range(m)]):
-            candidate = 1/n*np.trace(B0 @ Y)
-            if candidate< minimum_obj:
-                minimum_obj= candidate
-                solution = pert 
-                # print ("L: ", L/n)
-                # print  ("A: ", -A/n)
-    # print ("solution ", solution)
-    # print ("minimum objective ", minimum_obj)
-    return solution[0], L, A
+# def implement_brute_force(B0,B1,B2,B3,B4,B5): 
+#     ############## Implement brute force solution....
+#     A = lst = [list(i) for i in itertools.product([0, 1], repeat=n+n*m)]
+#     minimum_obj = 10000000000000 
+#     solution=[]
+#     # print (len(A))
+#     for pert in A:
+#         pert.append(1)
+#         pert = np.array([pert])
+#         Y = np.transpose(pert)*pert
+#         L = np.trace(B1 @ Y)
+#         A = np.trace(B2 @ Y)
+#         if L < Lmax*n and A <= -Amin * n and all([np.trace(B3[i] @ Y) == 1 for i in range(n)]) and all ([np.trace(B4[i] @ Y) <= bmax[i] for i in range(m)]):
+#             candidate = 1/n*np.trace(B0 @ Y)
+#             if candidate< minimum_obj:
+#                 minimum_obj= candidate
+#                 solution = pert 
+#                 # print ("L: ", L/n)
+#                 # print  ("A: ", -A/n)
+#     # print ("solution ", solution)
+#     # print ("minimum objective ", minimum_obj)
+#     return solution[0], L, A
 
 #geniki synartisi kostous gia X kai tin trexw kai gia to r0 kai gia to X_0
 def calculate_cost(solution, r):
@@ -817,10 +835,10 @@ def calculate_cost(solution, r):
     e_syn = ecomp + etr
     total_cost = lt * maxtk + le * e_syn  
     # print (total_cost)
-    print("this is maxtk",  maxtk)
-    print("this is etr",  etr)
-    print("this is ecomp",  ecomp)
-    print("this is pert", pert[0])
+    # print("this is maxtk",  maxtk)
+    # print("this is etr",  etr)
+    # print("this is ecomp",  ecomp)
+    # print("this is pert", pert[0])
     return total_cost
 
 def main():
